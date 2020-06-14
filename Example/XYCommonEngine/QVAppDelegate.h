8c298d6d1040eb29345dbb9416d2a36e8092ffa0
Sunshine <cheng.xia@quvideo.com>
2020-06-14 16:51:51 +0800

    add framework

---
+++ Example/XYCommonEngine/QVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QVAppDelegate.h
+//  XYCommonEngine
+//
+//  Created by Sunshine on 06/14/2020.
+//  Copyright (c) 2020 Sunshine. All rights reserved.
+//
+
+@import UIKit;
+
+@interface QVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XYCommonEngine/QVViewController.h
@@ -0,0 +1,13 @@
+//
+//  QVViewController.h
+//  XYCommonEngine
+//
+//  Created by Sunshine on 06/14/2020.
+//  Copyright (c) 2020 Sunshine. All rights reserved.
+//
+
+@import UIKit;
+
+@interface QVViewController : UIViewController
+
+@end
